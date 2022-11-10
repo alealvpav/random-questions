9df6dc2a3bd420c306b6539f02d63c51106d7ace
Alejandro Alvarez <alejandro.alvarezpavon@otainsight.com>
2022-11-10 14:05:17 +0100

    Add basic models

---
+++ questions/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Question(models.Model):
+    question = models.CharField(max_length=300)
+
+    def __str__(self):
+        q_mark = "" if self.question[:-1] == "?" else "?"
+        return f"{self.question}{q_mark}"
+
+
+class Answer(models.Model):
+    answer = models.TextField(max_length=1000)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    person = models.CharField(max_length=100, blank=True, default="Anonymous")
+
+    def __str__(self):
+        return f"\"{self.answer} ({self.person}).\""
